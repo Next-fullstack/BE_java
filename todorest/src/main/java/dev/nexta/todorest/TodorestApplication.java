8bd09b41c220214e93507240a3668a38302270e7
ekastn <118880703+ekastn@users.noreply.github.com>
2025-01-29 14:24:12 +0700

    feat: initialize spring boot

---
+++ todorest/src/main/java/dev/nexta/todorest/TodorestApplication.java
@@ -0,0 +1,13 @@
+package dev.nexta.todorest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodorestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodorestApplication.class, args);
+	}
+
+}
